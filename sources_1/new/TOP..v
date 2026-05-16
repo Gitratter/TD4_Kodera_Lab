ea1d16b0f8f3ebd516bd8acbb0b1087e54ad7c7d
Gitratter <23t3037@stu.meisei-u.ac.jp>
2026-05-17 03:12:03 +0900

    Update TOP..v

--- sources_1/new/TOP..v
+++ sources_1/new/TOP..v
@@ -23,7 +23,38 @@ assign LEDR = td4_inst.ADDR;
 endmodule 
 
 /*
-module TOP(clk, reset, in, out, clksel);
+module TD4_TOP(clk, reset, in, out, clksel, clk_ind);
     input clk, reset, clksel;
-    input [3:0] in,
+    input [3:0]in;
+    output reg [3:0]clk_ind;
     output reg [3:0]out = 4'b0000;
+
+    //clk switch
+    wire clk_1Hz, clk_10Hz, clkt;
+    assign clkt = (clksel == 1'b1)? clk_10Hz : clk_1Hz;
+
+    //clk generate
+    gen_1Hz gen1 (.clk(clk),.reset(reset), .clk_1Hz(clk_1Hz));
+    gen_10Hz gen10 (.clk(clk),.reset(reset), .clk_10Hz(clk_10Hz));
+
+    //pg counter reg
+    reg
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
