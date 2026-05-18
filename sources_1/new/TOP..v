b91da51af38d0586e7a0e06d5a997355512063e1
Gitratter <23t3037@stu.meisei-u.ac.jp>
2026-05-19 00:59:22 +0900

    Update TOP..v

--- sources_1/new/TOP..v
+++ sources_1/new/TOP..v
@@ -3,7 +3,8 @@ module TD4_TOP(clk, reset, in, out, clksel, clk_ind);
     input [3:0]in;
     output reg [3:0]clk_ind;
     output reg [3:0]out;
-
+    
+    /*
     //クロック切替
     wire clk_1Hz, clk_10Hz, clkt;
     assign clkt = (clksel == 1'b1)? clk_10Hz : clk_1Hz;
@@ -12,18 +13,48 @@ module TD4_TOP(clk, reset, in, out, clksel, clk_ind);
     gen_1Hz gen1 (.clk(clk),.reset(reset), .clk_1Hz(clk_1Hz));
     gen_10Hz gen10 (.clk(clk),.reset(reset), .clk_10Hz(clk_10Hz));
 
-    //プログラムカウンタ・レジスタ関連
+    */
+    
+    
+    //==================================================
+    // Enable Signal
+    //==================================================
+    wire enable_1Hz;
+    wire enable_10Hz;
+    wire enable_cpu;
+
+    assign enable_cpu = (clksel) ? enable_10Hz : enable_1Hz;
+
+    gen_1Hz_enable gen1(
+        .clk(clk),
+        .reset(reset),
+        .enable_1Hz(enable_1Hz)
+    );
+
+    gen_10Hz_enable gen10(
+        .clk(clk),
+        .reset(reset),
+        .enable_10Hz(enable_10Hz)
+    );
+    
+    //==================================================
+    // Register
+    //==================================================
     reg [3:0]reg_A;
     reg [3:0]reg_B;  //register
     reg [3:0]pcnt;  //program count
     reg carry_flag;
     wire carry_wire;
 
-    //ROM---------
+    //==================================================
+    // ROM
+    //==================================================
     wire [7:0] command;
     ROM rom(.pcnt(pcnt), .command(command));
 
-    //Decoder---------
+    //==================================================
+    // Decoder
+    //==================================================
     wire [6:0] instr;
     wire [3:0] opcode = command[7:4];
     wire [3:0] im = command[3:0];
@@ -36,35 +67,59 @@ module TD4_TOP(clk, reset, in, out, clksel, clk_ind);
     wire LOAD3 = instr[1];
     wire ALUsel = instr[0];
 
-    //Selecter---------
+    //==================================================
+    // Selector
+    //==================================================
     wire [3:0]select_data;
     SELECTOR selector(.select({selectB, selectA}), .reg_A(reg_A), .reg_B(reg_B), .in(in), .select_data(select_data));
 
-    //ALU----------
+    //==================================================
+    // ALU
+    //==================================================
     wire [3:0]ALU_data;
     ALU alu(.ALUsel(ALUsel), .select_data(select_data), .im(im), .ALU_data(ALU_data), .carry(carry_wire));
 
-    //Register---------
-    always@(posedge clkt or posedge reset)begin
+    //==================================================
+    // CPU Register Update
+    //==================================================
+    always@(posedge clk or posedge reset)begin
         if(reset)begin
-            reg_A <= 0;
-            reg_B <= 0;
-            out <= 0;
-            pcnt <= 0;
-            carry_flag <= 0;
-            clk_ind <= 0;
-        end else begin
-            if(~LOAD0)reg_A <= ALU_data;
-            if(~LOAD1)reg_B <= ALU_data;
-            if(~LOAD2)out <= ALU_data;
-            pcnt <= ~LOAD3? ALU_data : pcnt + 1'b1;
+            reg_A      <= 4'b0000;
+            reg_B      <= 4'b0000;
+            out        <= 4'b0000;
+            pcnt       <= 4'b0000;
+            carry_flag <= 1'b0;
+            clk_ind    <= 4'b0000;
+
+        end
+
+        else if(enable_cpu) begin
+
+            // Register Write
+
+            if(~LOAD0)
+                reg_A <= ALU_data;
+
+            if(~LOAD1)
+                reg_B <= ALU_data;
+
+            if(~LOAD2)
+                out <= ALU_data;
+
+            // Program Counter
+            if(~LOAD3)
+                pcnt <= ALU_data;
+            else
+                pcnt <= pcnt + 1'b1;
+
+            // Carry Flag
             carry_flag <= carry_wire;
 
             //------clock Indicator
             if(clk_ind >= 4'b1111)
-                clk_ind <= 0;
+                clk_ind <= 4'b0000;
             else
-                clk_ind <= clk_ind + 1;
+                clk_ind <= clk_ind + 1'b1;
             //------clock Indicator
         end
 
