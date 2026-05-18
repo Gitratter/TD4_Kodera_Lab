7af87fce0ffbc5d2628a1c9f31773f74f712138d
Gitratter <23t3037@stu.meisei-u.ac.jp>
2026-05-19 01:13:14 +0900

    Update 1Hz.v

--- sources_1/new/1Hz.v
+++ sources_1/new/1Hz.v
@@ -1,18 +1,27 @@
 //1Hz 発生
-module gen_1Hz(clk, reset, clk_1Hz);
+module gen_1Hz_enable(clk, reset, enable_1Hz);
     input clk, reset;
-    output reg clk_1Hz;
+    output reg enable_1Hz;
 
     reg[26:0]count;
     always@(posedge clk or posedge reset)begin
-        if(reset == 1'b1)begin
+		
+        if(reset) begin
+
             count <= 0;
-            clk_1Hz <= 0;
-        end else begin
-            count <= count + 1'b1;
-			if(count == 27'd50_000_000)begin
-                clk_1Hz <= ~clk_1Hz;
-                count <= 27'b0;
+            enable_1Hz <= 0;
+        end
+        else begin
+            if(count == 27'd50_000_000 - 1) begin
+
+                count <= 0;
+                enable_1Hz <= 1'b1;
+
+            end
+            else begin
+                count <= count + 1'b1;
+                enable_1Hz <= 1'b0;
+				
             end
         end
     end
