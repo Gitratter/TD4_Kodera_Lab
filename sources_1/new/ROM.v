bf2cb3439622d3654715b35209ba3e634f1055c6
Gitratter <23t3037@stu.meisei-u.ac.jp>
2026-05-19 01:03:15 +0900

    Update and rename ROM2.v to ROM.v

--- sources_1/new/ROM.v
+++ sources_1/new/ROM.v
@@ -3,7 +3,15 @@ module ROM(pcnt, command);
     output [7:0]command;
 
 	reg [7:0]rom[0:15];
+
+	integer i;
     initial begin
+		
+		// Default
+        for(i = 0; i < 16; i = i + 1)
+            rom[i] = 8'b11110000;
+
+        // Program
         rom[0] = 8'b00000001;  //add regA+1
         rom[1] = 8'b10100000;  //regA out
         rom[2] = 8'b00000001;  //add regA+1
@@ -12,6 +20,7 @@ module ROM(pcnt, command);
         rom[5] = 8'b10100000;  //regA out
         rom[6] = 8'b10000010;  //sub regA-10
         rom[7] = 8'b10100000;  //regA out
+		
     end
     assign command = rom[pcnt];
 endmodule
